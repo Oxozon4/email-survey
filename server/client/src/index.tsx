1a984ec8f0d834120c994fc8b1a55d0f448ac149
Oxozon <oxozon.o4@gmail.com>
2022-12-05 23:43:43 +0100

    init redux store

--- server/client/src/index.tsx
+++ server/client/src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el!);
+
+const store = createStore(reducers, {}, applyMiddleware());
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
---
+++ server/client/src/reducers/authReducer.ts
@@ -0,0 +1,6 @@
+export const authReducer = (state = {}, action: any): typeof state => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
