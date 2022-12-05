5fad9044d2502aa4c9e6454caed726ec526fd8ba
Oxozon <oxozon.o4@gmail.com>
2022-12-05 23:51:25 +0100

    init react router

--- server/client/src/components/App.tsx
+++ server/client/src/components/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/survey" component={Dashboard}></Route>
+          <Route path="/survey/new" component={SurveyNew}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
