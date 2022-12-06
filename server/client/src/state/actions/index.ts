6e370747a9d87447f50b96a93d66341d05cea3d1
Oxozon <oxozon.o4@gmail.com>
2022-12-07 00:15:59 +0100

    add fetchUser action type

--- server/client/src/state/actions/index.ts
+++ server/client/src/state/actions/index.ts
@@ -1,20 +1,20 @@
 import { ActionType } from '../action-types';
 
-interface GetAdvancedLinksAction {
-  // type: ActionType.GET_ADVANCED_LINKS;
-}
+// interface GetAdvancedLinksAction {
+//   // type: ActionType.GET_ADVANCED_LINKS;
+// }
 
-interface GetAdvancedLinksSuccessAction {
-  // type: ActionType.GET_ADVANCED_LINKS_SUCCESS;
-  payload: { rel: string; href: string }[];
-}
+// interface GetAdvancedLinksSuccessAction {
+//   // type: ActionType.GET_ADVANCED_LINKS_SUCCESS;
+//   payload: { rel: string; href: string }[];
+// }
 
-interface GetAdvancedLinksErrorAction {
-  // type: ActionType.GET_ADVANCED_LINKS_ERROR;
-  payload: string;
-}
+// interface GetAdvancedLinksErrorAction {
+//   // type: ActionType.GET_ADVANCED_LINKS_ERROR;
+//   payload: string;
+// }
 
-export type AdvancedLinksAction =
-  | GetAdvancedLinksAction
-  | GetAdvancedLinksSuccessAction
-  | GetAdvancedLinksErrorAction;
+export interface FetchUserAction {
+  type: ActionType.FETCH_USER;
+  payload: null | {} | false;
+}
--- server/client/src/state/reducers/authReducer/authReducer.ts
+++ server/client/src/state/reducers/authReducer/authReducer.ts
@@ -1,6 +1,18 @@
-export const authReducer = (state = {}, action: any): typeof state => {
+import { ActionType } from '../../action-types';
+import { FetchUserAction } from '../../actions';
+
+interface UserModel {}
+
+const initialState = null;
+
+export const authReducer = (
+  state = initialState,
+  action: FetchUserAction
+): null | UserModel | false => {
   console.log(action);
   switch (action.type) {
+    case ActionType.FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
