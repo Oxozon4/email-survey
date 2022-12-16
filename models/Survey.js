4df657b047f0e6f7b6052b048877d8ff38821f83
Oxozon <oxozon.o4@gmail.com>
2022-12-16 14:37:50 +0100

    add recipientSchema

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const recipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [recipientSchema],
   yes: { type: Number, defualt: 0 },
   no: { type: Number, defualt: 0 },
 });
