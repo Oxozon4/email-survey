695cc164fa2297870cfe4fda4c9e398783bd9a67
Oxozon <oxozon.o4@gmail.com>
2023-01-02 21:13:37 +0100

    add helper functions

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,6 +9,23 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSetting = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSetting.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSetting);
   }
 }
 
