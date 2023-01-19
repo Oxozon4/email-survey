269f72ca7570a9fc197526239b3c23f43b1ca578
Oxozon <oxozon.o4@gmail.com>
2023-01-19 20:04:56 +0100

    add SurveyField component, add renderField method

---
+++ client/src/components/surveys/SurveyField.tsx
@@ -0,0 +1,9 @@
+const SurveyField: React.FC = () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
+
+export default SurveyField;
--- client/src/components/surveys/SurveyForm.tsx
+++ client/src/components/surveys/SurveyForm.tsx
@@ -1,12 +1,22 @@
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 interface Props {}
 
 const SurveyForm: React.FC = ({ handleSubmit }: any) => {
+  const renderFields = () => {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit((values: any) => console.log(values))}>
-        <Field type="text" name="surveyTitle" component="input" />
+        {/* <Field type="text" name="surveyTitle" component="input" /> */}
+        {renderFields()}
         <button type="submit">Submit</button>
       </form>
     </div>
