b7a34b042b6a8fdcc57448407f58fe444d46d39b
Oxozon <oxozon.o4@gmail.com>
2023-01-19 20:14:28 +0100

    display all custom inputs

--- client/src/components/surveys/SurveyField.tsx
+++ client/src/components/surveys/SurveyField.tsx
@@ -1,8 +1,9 @@
 import { InjectedFormProps } from 'redux-form';
 
-const SurveyField: React.FC<InjectedFormProps> = ({ input, meta }: any) => {
+const SurveyField: React.FC<InjectedFormProps> = ({ input, label }: any) => {
   return (
     <div>
+      <label>{label}</label>
       <input {...input} />
     </div>
   );
