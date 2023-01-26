21b9d25658bccad3f43e3bebb239b60d2fe19269
Oxozon <oxozon.o4@gmail.com>
2023-01-26 22:13:59 +0100

    finish review fields

--- client/src/components/surveys/SurveyFormReview.tsx
+++ client/src/components/surveys/SurveyFormReview.tsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { formFields } from './formFields';
+import _ from 'lodash';
 
 interface SurveyFormReviewProps {
   onCancel: () => void;
@@ -7,10 +8,19 @@ interface SurveyFormReviewProps {
 }
 
 const SurveyFormReview = ({ onCancel, formValues }: SurveyFormReviewProps) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>{formFields.map()}</div>
+      <div>{reviewFields}</div>
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
