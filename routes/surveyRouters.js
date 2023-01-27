ae641aa748a291bb06953a618648d2a66a4c3ee2
Oxozon <oxozon.o4@gmail.com>
2023-01-27 14:51:14 +0100

    add lodash chain helper

--- routes/surveyRouters.js
+++ routes/surveyRouters.js
@@ -15,14 +15,23 @@ module.exports = (app) => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ url, email }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path(`/api/surveys/:surveyId/:choice`);
-      const match = p.test(pathname);
-      if (match) {
-        return { email: email, surveyId: match.surveyId, choice: match.choice };
-      }
-    });
+    const p = new Path(`/api/surveys/:surveyId/:choice`);
+
+    const events = _.chain(req.body)
+      .map(({ url, email }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email: email,
+            surveyId: match.surveyId,
+            choice: match.choice,
+          };
+        }
+      })
+
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
 
     console.log(events);
   });
